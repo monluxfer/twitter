44f73bb53605e3907c73ed6b457c4528aef23be9
Monluxfer <shibarome@gmail.com>
2020-05-26 02:34:49 +0300

    user tests

--- test/twitter/accounts/user_test.exs
+++ test/twitter/accounts/user_test.exs
@@ -6,29 +6,34 @@ defmodule Twitter.Accounts.UserTest do
     describe "changeset" do
 
         test "all data is valid" do
-            changeset = User.changeset(%User{}, %{ email: "some_email", password_hash: "some_password_hash", username: "some_username" })
+            changeset = User.changeset(%User{}, %{ email: "some@email", username: "some_username", password: "some_password", password_confirmation: "some_password" })
             assert changeset.errors == []
         end
 
         test "email is null" do
-            changeset = User.changeset(%User{}, %{ email: nil, password_hash: "some_password_hash", username: "some_username" })
-            assert changeset.errors == [email: {"can't be blank", [validation: :required]}]
-        end
-
-        test "password_hash is null" do
-            changeset = User.changeset(%User{}, %{ email: "some_email", password_hash: nil, username: "some_username" })
-            assert changeset.errors == [password_hash: {"can't be blank", [validation: :required]}]
+            changeset = User.changeset(%User{}, %{ email: nil, username: "some_username", password: "some_password", password_confirmation: "some_password" })
+            assert changeset.errors == [ email: {"can't be blank", [validation: :required]} ]
         end
 
         test "username is null" do
-            changeset = User.changeset(%User{}, %{ email: "some_email", password_hash: "some_password_hash", username: nil })
-            assert changeset.errors == [username: {"can't be blank", [validation: :required]}]
+            changeset = User.changeset(%User{}, %{ email: "some@email", username: nil, password: "some_password", password_confirmation: "some_password" })
+            assert changeset.errors == [ username: {"can't be blank", [validation: :required]} ]
+        end
+
+        test "password is null" do
+            changeset = User.changeset(%User{}, %{ email: "some@email", username: "some_username", password: nil, password_confirmation: nil })
+            assert changeset.errors == [ password: {"can't be blank", [validation: :required]}, password_confirmation: {"can't be blank", [validation: :required]} ]
+        end
+
+        test "password != password_confirmation" do
+            changeset = User.changeset(%User{}, %{ email: "some@email", username: "some_username", password: "firstpass", password_confirmation: "secondpass" })
+            assert changeset.errors == [ password_confirmation: {"does not match confirmation", [validation: :confirmation]} ]
         end
 
         test "all data is null" do
-            changeset = User.changeset(%User{}, %{ email: nil, password_hash: nil, username: nil })
-            assert changeset.errors == [email: {"can't be blank", [validation: :required]}, password_hash: {"can't be blank",
-            [validation: :required]}, username: {"can't be blank", [validation: :required]}]
+            changeset = User.changeset(%User{}, %{ email: nil, username: nil, password: nil, password_confirmation: nil })
+            assert changeset.errors == [ email: {"can't be blank", [validation: :required]}, username: {"can't be blank",
+            [validation: :required]}, password: {"can't be blank", [validation: :required]}, password_confirmation: {"can't be blank", [validation: :required]} ]
         end
 
     end
