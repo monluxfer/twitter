36a3887f32f1aebbbac65b87cf14e42772b1fd4e
Monluxfer <shibarome@gmail.com>
2020-06-02 16:16:58 +0300

    uncommentary

--- test/twitter_web/controllers/users_controller_test.exs
+++ test/twitter_web/controllers/users_controller_test.exs
@@ -5,12 +5,11 @@ defmodule TwitterWeb.UsersControllerTest do
 
     describe "create" do
 
-        # test "creates a user", %{conn: conn} do
-        #     attrs = %{email: "some@email", username: "some_username", password: "password", password_confirmation: "password"}
-        #     conn = post(conn, Routes.users_path(conn, :create), attrs)
-        #     assert "some_email" == json_response(conn, 201)["jwt"]
-        #     IO.puts(json_response(conn, 201)["jwt"])
-        # end
+        test "creates a user", %{conn: conn} do
+            attrs = %{email: "some@email", username: "some_username", password: "password", password_confirmation: "password"}
+            conn = post(conn, Routes.users_path(conn, :create), attrs)
+            IO.puts(json_response(conn, 201)["jwt"])
+        end
 
         test "creates a follower", %{conn: conn} do
             user1 = insert(:user)
